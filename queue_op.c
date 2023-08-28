8746fa1e06e7a90fa03393cf86b3ddcf45d2a653
Jaybabson <yahayajoel2@gmail.com>
2023-08-27 17:41:08 -0700

    monty opcode files

---
+++ queue_op.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * f_queue - This prints the top
+ * @head: The stack head
+ * @counter: number
+ * Return: void
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - This adds a node to the tail stack
+ * @n: a new_value
+ * @head: the head of the stack
+ * Return: void
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
---
+++ stack_op.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - Sets the format to stack
+ * @head: The stack head
+ * @counter: number
+ * Return: void
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
